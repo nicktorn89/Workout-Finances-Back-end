6baf6e9b2e8586bb37b655eccacc7720314cdf8b
vlad-komanchy <vkomanchy@gmail.com>
2019-07-27 17:05:51 +0300

    feat: add controller and provider for workouts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WorkoutsController } from './workouts/workouts.controller';
+import { WorkoutsService } from './workouts/workouts.service';
+import { CatsService } from './cats/cats.service';
 
 @Module({
   imports: [],
   controllers: [AppController, WorkoutsController],
-  providers: [AppService],
+  providers: [AppService, WorkoutsService, CatsService],
 })
 export class AppModule {}
