ae24c4dcceeccdf11a126694d5b218fe63dfbddb
vlad-komanchy <vkomanchy@gmail.com>
2019-07-27 14:01:04 +0300

    feat: add first controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { WorkoutsController } from './workouts/workouts.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, WorkoutsController],
   providers: [AppService],
 })
 export class AppModule {}
