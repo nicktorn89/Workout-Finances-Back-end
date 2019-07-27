6d424d93ba59e473c1969537889cbaa3f6a37e65
vlad-komanchy <vkomanchy@gmail.com>
2019-07-27 17:05:51 +0300

    feat: add controller and provider for workouts

--- src/workouts/workouts.controller.ts
+++ src/workouts/workouts.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { WorkoutsService } from './workouts.service';
 
 @Controller('workouts')
-export class WorkoutsController {}
+export class WorkoutsController {
+  constructor(private readonly workoutsService: WorkoutsService) {}
+
+  @Get()
+  getHello(): string {
+    return this.workoutsService.getHello();
+  }
+}
