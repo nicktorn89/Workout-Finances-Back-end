974f5a87b5882fd27f56a52d6ca2a3506d636655
vlad-komanchy <vkomanchy@gmail.com>
2019-08-03 20:56:21 +0300

    feat: add Mongoose, requests to workouts

--- src/workouts/workouts.controller.ts
+++ src/workouts/workouts.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { WorkoutsService } from './workouts.service';
+import { CreateWorkoutDto } from './dto/create-workout.dto';
+import { Workout } from './interfaces/workouts.interface';
 
 @Controller('workouts')
 export class WorkoutsController {
   constructor(private readonly workoutsService: WorkoutsService) {}
 
+  @Post()
+  async create(@Body() createWorkoutDto: CreateWorkoutDto) {
+    // return 'this actions create workout';
+    await this.workoutsService.create(createWorkoutDto);
+  }
+
   @Get()
-  getHello(): string {
-    return this.workoutsService.getHello();
+  findAll(): Promise<Workout[]> {
+    return this.workoutsService.findAll();
   }
 }
--- src/workouts/workouts.service.ts
+++ src/workouts/workouts.service.ts
@@ -1,8 +1,20 @@
+import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+
+import { Workout } from './interfaces/workouts.interface';
+import { CreateWorkoutDto } from './dto/create-workout.dto';
 
 @Injectable()
 export class WorkoutsService {
-  getHello(): string {
-    return 'This is workouts';
+  constructor(@InjectModel('Workout') private readonly workoutModel: Model<Workout>) {}
+
+  async create(workout: CreateWorkoutDto): Promise<Workout> {
+    const createdWorkout = new this.workoutModel(workout);
+    return await createdWorkout.save();
+  }
+
+  async findAll(): Promise<Workout[]> {
+    return await this.workoutModel.find().exec();
   }
 }
