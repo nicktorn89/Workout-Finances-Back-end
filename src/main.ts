406eebf678b9959bf601ec747f7a1e3d04fd300f
vlad-komanchy <vkomanchy@gmail.com>
2019-08-04 14:16:46 +0300

    feat: add delete and edit requests
    - Return all workouts after requests

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const corsOptions = { 
+    origin: 'http://localhost:8080',
+    credentials: true,
+  };
+  
+  app.enableCors(corsOptions);
   await app.listen(3003);
 }
 bootstrap();
--- src/workouts/workouts.service.ts
+++ src/workouts/workouts.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 
 import { Workout } from './interfaces/workouts.interface';
-import { CreateWorkoutDto } from './dto/create-workout.dto';
+import { CreateWorkoutDto, RemoveWorkoutDto, EditWorkoutDto } from './dto/create-workout.dto';
 
 @Injectable()
 export class WorkoutsService {
@@ -12,6 +12,20 @@ export class WorkoutsService {
   async create(workout: CreateWorkoutDto): Promise<Workout> {
     const createdWorkout = new this.workoutModel(workout);
     return await createdWorkout.save();
+  }
+
+  async edit(workout: EditWorkoutDto) {
+    await this.workoutModel.findOneAndUpdate({ _id: workout._id}, workout).exec();
+  }
+
+  async remove({ idArray }: RemoveWorkoutDto) {
+    if (idArray.length === 0) return;
+
+    if (idArray.length > 1) {
+      await this.workoutModel.deleteMany({ _id: idArray }).exec();
+    } else {
+      await this.workoutModel.deleteOne({ _id: idArray[0] }).exec();
+    }
   }
 
   async findAll(): Promise<Workout[]> {
